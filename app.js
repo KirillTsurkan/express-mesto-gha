b7099ffafd47d1dfdac4481feac04b7660b59021
Kirill TSurkan <tsurkan2019@yandex.ru>
2022-04-21 17:37:42 +0300

    fix: удаление комментариев при разработке

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const app = express();
 const bodyParser = require('body-parser');
 
 //  Методы для работы с пакетами
-app.use(bodyParser.json()); // для собирания JSON-формата
-app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Подключаем роуты
 const usersRoute = require('./routes/users');
@@ -29,7 +29,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 // id пользователя
 app.use((req, res, next) => {
   req.user = {
-    _id: '62568e512082c7c0e4ba8033', // вставляем сюда _id созданного в postman пользователя
+    _id: '62568e512082c7c0e4ba8033',
   };
   next();
 });
